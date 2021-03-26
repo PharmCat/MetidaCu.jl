e2938203ff9c557a243d46a5d263873880c0e03e
PharmCat <v.s.arnautov@yandex.ru>
2021-03-26 15:10:39 +0300

    v0.3.0

--- src/MetidaCu.jl
+++ src/MetidaCu.jl
@@ -2,8 +2,7 @@ module MetidaCu
     using LinearAlgebra, CUDA
     import MetidaNLopt, Metida
     import MetidaNLopt: reml_sweep_β_cuda#, cudata
-    import Metida: LMM, AbstractLMMDataBlocks, rmat_base_inc!, zgz_base_inc!
-    #using TimerOutputs
+    import Metida: LMM, AbstractLMMDataBlocks, rmat_base_inc!, zgz_base_inc!, logerror!
 
     struct LMMDataBlocks{T1, T2} <: AbstractLMMDataBlocks
         # Fixed effect matrix views
@@ -33,7 +32,6 @@ module MetidaCu
     end
 
     function MetidaNLopt.reml_sweep_β_cuda(lmm::LMM, data::AbstractLMMDataBlocks, θ::Vector{T}) where T
-        #to = TimerOutput()
         n             = length(lmm.covstr.vcovblock)
         N             = length(lmm.data.yv)
         c             = (N - lmm.rankx)*log(2π)
@@ -45,63 +43,40 @@ module MetidaCu
         βtc           = CUDA.zeros(T, lmm.rankx)
         β             = Vector{T}(undef, lmm.rankx)
         A             = Vector{CuArray{T, 2}}(undef, n)
-        #X             = Vector{CuArray{T, 2}}(undef, n)
-        #y             = Vector{CuArray{T, 1}}(undef, n)
-        q             = zero(Int)
-        qswm          = zero(Int)
         logdetθ₂      = zero(T)
-        @inbounds for i = 1:n
-            q    = length(lmm.covstr.vcovblock[i])
-            qswm = q + lmm.rankx
-            #@timeit to "V matrix" begin
-            V    = zeros(T, q, q)
-            Metida.zgz_base_inc!(V, θ, lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
-            Metida.rmat_base_inc!(V, θ[lmm.covstr.tr[end]], lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
-            #end
-            #@timeit to "CuArray"
-            A[i] = CuArray(V)
-            #X[i] = CuArray(view(lmm.data.xv,  lmm.covstr.vcovblock[i], :))
-            #y[i] = CuArray(view(lmm.data.yv, lmm.covstr.vcovblock[i]))
+        try
+            @inbounds @simd for i = 1:n
+                q    = length(lmm.covstr.vcovblock[i])
+                qswm = q + lmm.rankx
+                V    = zeros(T, q, q)
+                Metida.zgz_base_inc!(V, θ, lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
+                Metida.rmat_base_inc!(V, θ[lmm.covstr.tr[end]], lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
+                A[i] = CuArray(V)
             #-------------------------------------------------------------------
-            #Cholesky
-            #@timeit to "potrf! 1"
-            A[i] = LinearAlgebra.LAPACK.potrf!('L', A[i])[1]
-            try
-                #@timeit to "logdet 1" begin
-                    θ₁  += sum(log.(diag(A[i])))*2
-                #end
-                #θ₁  += logdet(Cholesky(Matrix(A[i]), 'L', 0))
-            catch
-                lmmlog!(lmm, LMMLogMsg(:ERROR, "θ₁ not estimated during REML calculation, V isn't positive definite or |V| less zero."))
-                return (1e100, nothing, nothing, 1e100)
+            # Cholesky
+                A[i] = LinearAlgebra.LAPACK.potrf!('U', A[i])[1]
+                θ₁  += sum(log.(diag(A[i])))*2
+                vX   = LinearAlgebra.LAPACK.potrs!('U', A[i], copy(data.xv[i]))
+                vy   = LinearAlgebra.LAPACK.potrs!('U', A[i], copy(data.yv[i]))
+                CUDA.CUBLAS.gemm!('T', 'N', one(T), data.xv[i], vX, one(T), θ₂tc)
+                CUDA.CUBLAS.gemv!('T', one(T), data.xv[i], vy, one(T), βtc)
+            #-------------------------------------------------------------------
             end
-            #@timeit to "potrs! 1"
-            vX   = LinearAlgebra.LAPACK.potrs!('L', A[i], copy(data.xv[i]))
-            #@timeit to "potrs! 2"
-            vy   = LinearAlgebra.LAPACK.potrs!('L', A[i], copy(data.yv[i]))
-            #@timeit to "gemm! 1"
-            CUDA.CUBLAS.gemm!('T', 'N', one(T), data.xv[i], vX, one(T), θ₂tc)
-            #@timeit to "gemv! 1"
-            CUDA.CUBLAS.gemv!('T', one(T), data.xv[i], vy, one(T), βtc)
-            #-------------------------------------------------------------------
-        end
-        #Beta calculation
-        #@timeit to "θ₂"
-        copyto!(θ₂, θ₂tc)
-        LinearAlgebra.LAPACK.potrf!('L', θ₂tc)
-        #@timeit to "β"
-        copyto!(β, LinearAlgebra.LAPACK.potrs!('L', θ₂tc, βtc))
+        # Beta calculation
+            copyto!(θ₂, θ₂tc)
+            LinearAlgebra.LAPACK.potrf!('U', θ₂tc)
+            copyto!(β, LinearAlgebra.LAPACK.potrs!('U', θ₂tc, βtc))
         # θ₃ calculation
-        @simd for i = 1:n
-            #@timeit to "gemv! 2"
-            r = CUDA.CUBLAS.gemv!('N', -one(T), data.xv[i], βtc, one(T), copy(data.yv[i]))
-            #@timeit to "potrs! 3"
-            vr   = LinearAlgebra.LAPACK.potrs!('L', A[i], copy(r))
-            θ₃  += r'*vr
+            @inbounds @simd for i = 1:n
+                r = CUDA.CUBLAS.gemv!('N', -one(T), data.xv[i], βtc, one(T), copy(data.yv[i]))
+                vr   = LinearAlgebra.LAPACK.potrs!('U', A[i], copy(r))
+                θ₃  += r'*vr
+            end
+            logdetθ₂ = logdet(θ₂)
+        catch e
+            logerror!(e, lmm)
+            return (Inf, nothing, nothing, nothing, false)
         end
-        #@timeit to "logdet 2"
-        logdetθ₂ = logdet(θ₂)
-        #println(to)
-        return   θ₁ + logdetθ₂ + θ₃ + c, β, θ₂, θ₃
+        return   θ₁ + logdetθ₂ + θ₃ + c, β, θ₂, θ₃, true
     end
 end # module
