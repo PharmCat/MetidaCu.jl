20ed62d89a09cb6a57cf95c2a6a9d587307a5928
PharmCat <v.s.arnautov@yandex.ru>
2021-11-04 00:38:16 +0300

    v0.4.0

--- src/MetidaCu.jl
+++ src/MetidaCu.jl
@@ -2,7 +2,7 @@ module MetidaCu
     using LinearAlgebra, CUDA
     import MetidaNLopt, Metida
     import MetidaNLopt: reml_sweep_β_cuda#, cudata
-    import Metida: LMM, AbstractLMMDataBlocks, rmat_base_inc!, zgz_base_inc!, logerror!
+    import Metida: LMM, AbstractLMMDataBlocks, rmat_base_inc!, zgz_base_inc!
 
     struct LMMDataBlocks{T1, T2} <: AbstractLMMDataBlocks
         # Fixed effect matrix views
@@ -43,21 +43,35 @@ module MetidaCu
         βtc           = CUDA.zeros(T, lmm.rankx)
         β             = Vector{T}(undef, lmm.rankx)
         A             = Vector{CuArray{T, 2}}(undef, n)
+        V             = Vector{Array{T, 2}}(undef, n)
         logdetθ₂      = zero(T)
-        try
-            @inbounds @simd for i = 1:n
+        noerror       = true
+        #try
+
+            Base.Threads.@threads for i = 1:n
                 q    = length(lmm.covstr.vcovblock[i])
                 qswm = q + lmm.rankx
-                V    = zeros(T, q, q)
-                Metida.zgz_base_inc!(V, θ, lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
-                Metida.rmat_base_inc!(V, θ[lmm.covstr.tr[end]], lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
-                A[i] = CuArray(V)
+                V[i]   = zeros(T, q, q)
+                Metida.zgz_base_inc!(V[i], θ, lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
+                Metida.rmat_base_inc!(V[i], θ[lmm.covstr.tr[end]], lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
+            end
+
+            @inbounds @simd for i = 1:n
+                A[i] = CuArray(V[i])
             #-------------------------------------------------------------------
             # Cholesky
-                A[i] = LinearAlgebra.LAPACK.potrf!('U', A[i])[1]
-                θ₁  += sum(log.(diag(A[i])))*2
+                A[i], info = LinearAlgebra.LAPACK.potrf!('U', A[i])
                 vX   = LinearAlgebra.LAPACK.potrs!('U', A[i], copy(data.xv[i]))
                 vy   = LinearAlgebra.LAPACK.potrs!('U', A[i], copy(data.yv[i]))
+                if info == 0
+                    θ₁ld = sum(log.(diag(A[i])))*2
+                    ne = true
+                else
+                    θ₁ld, ne = logdet_(Vector(diag(A[i])))
+                end
+                if ne == false noerror = false end
+                #θ₁  += sum(log.(diag(A[i])))*2
+                θ₁  += θ₁ld
                 CUDA.CUBLAS.gemm!('T', 'N', one(T), data.xv[i], vX, one(T), θ₂tc)
                 CUDA.CUBLAS.gemv!('T', one(T), data.xv[i], vy, one(T), βtc)
             #-------------------------------------------------------------------
@@ -73,10 +87,27 @@ module MetidaCu
                 θ₃  += r'*vr
             end
             logdetθ₂ = logdet(θ₂)
-        catch e
-            logerror!(e, lmm)
-            return (Inf, nothing, nothing, nothing, false)
-        end
-        return   θ₁ + logdetθ₂ + θ₃ + c, β, θ₂, θ₃, true
+        #catch e
+        #    logerror!(e, lmm)
+        #    return (Inf, nothing, nothing, nothing, false)
+        #end
+        return   θ₁ + logdetθ₂ + θ₃ + c, β, θ₂, θ₃, noerror
     end
+
+    function logdet_(vec::AbstractVector{T}) where T
+        dd = zero(T)
+        noerror = true
+        @inbounds for i in 1:length(vec)
+            v = vec[i]
+            if v > 0
+                dd += log(v)
+            else
+                dd += log(vec[i] + 4eps())
+                noerror = false
+            end
+        end
+        dd + dd, noerror
+    end
+
+
 end # module
